8fcb37aaf8ef72276ab3879dba4c92cfc50bee76
Behrouz Safari <behrouz.safari@gmail.com>
2023-01-13 06:44:49 +0100

    nothing

--- 02.py
+++ 02.py
@@ -5,7 +5,7 @@ from funcs import *
 sp.furnsh('kernel.tm')
 #=====================================================
 
-ets, ras, decs, rasJ2000, decsJ2000, inds = equinox(year=-5000, steps=10000)
+ets, ras, decs, rasJ2000, decsJ2000, inds = equinox(year=-3000, steps=10000)
 i_spring, i_summer, i_autumn, i_winter = inds
 
 print('Vernal equinox J2000:', (rasJ2000[i_spring], decsJ2000[i_spring]))
