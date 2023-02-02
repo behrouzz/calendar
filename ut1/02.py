0a96165f210412b6c4738c69e7feef81e4b63f2c
Behrouz <behrouz.safari@gmail.com>
2023-02-02 17:59:30 +0100

    Add files via upload

---
+++ ut1/02.py
@@ -0,0 +1,38 @@
+import spiceypy as sp
+import numpy as np
+from scipy import interpolate
+from scipy.optimize import fsolve
+from hypatie import car2sph
+import pickle
+from glob import glob
+import pandas as pd
+
+
+def dec_sun(et):
+    # J2000
+    pos, _ = sp.spkez(10, et, 'J2000', 'LT+S', 399)
+    sun_2000 = pos[:3] #GCRS
+    _, dec_2000, _ = car2sph(sun_2000)
+    rotmat = sp.sxform('J2000', 'TETE', et)[:3,:3]
+    # equinox of date
+    sun_date = np.matmul(rotmat, sun_2000)
+    _, dec_date, _ = car2sph(sun_date)
+    return dec_2000, dec_date
+
+
+ET, TIME, GY, GM, GD, PY = pd.read_csv('data/equinox_time.csv').iloc[5000]
+JD = (ET/86400) + 2451545.0
+
+
+sp.furnsh('C:/Moi/_py/Astronomy/Solar System/kernels/heavy.tm')
+
+print(ET)
+s = sp.etcal(ET)
+print(s)
+new_et = sp.str2et(s + ' UTC')
+print(new_et)
+new_s = sp.etcal(new_et)
+print(new_s)
+
+sp.kclear()
+
---
+++ ut1/03_tt_ut1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from iers import create_df, files_dc
+import matplotlib.pyplot as plt
+
+LOCAL = 'C:/Moi/_py/Astronomy/Earth/IERS/data/'
+file = LOCAL + files_dc['hmnao_a_2021.eop']['adr'] + 'hmnao_a_2021.eop'
+df = create_df(file)[['MJD','UT1-TAI']]
+df['jd'] = df['MJD'] + 2400000.5
+# tt = tai + 32.184
+df['UT1-TT'] = df['UT1-TAI'] - 32.184
+
+plt.plot(df['jd'], df['UT1-TT'])
+plt.show()
